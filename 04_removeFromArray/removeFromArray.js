0647e5322627b0c19f70a89d61a7e39747cdf7ba
Tshai Ben Shabo <nightcn@gmail.com>
2023-11-24 20:58:41 +0200

    Solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function (...args) {
+  let newArr = args[0].filter((item) => {
+    return !args.slice(1).includes(item);
+  });
 
+  return newArr;
 };
 
 // Do not edit below this line
