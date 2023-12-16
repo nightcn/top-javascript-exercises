c2fc301cb79293ce8be8418e30846b6b8a7bf75b
TBS-PC <tsahibspele@gmail.com>
2023-12-16 11:04:20 +0200

    Solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,25 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((acc, num) => acc + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((acc, num) => acc * num);
 };
 
-const power = function() {
-	
+const power = function (a, p) {
+  return a ** p;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  return num <= 1 ? 1 : num * factorial(num - 1);
 };
 
 // Do not edit below this line
@@ -29,5 +29,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
