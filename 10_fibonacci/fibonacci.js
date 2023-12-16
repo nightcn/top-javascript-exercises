b96b06aeb954bc00aa23e58f8e6ecb0a80ca6c33
TBS-PC <tsahibspele@gmail.com>
2023-12-16 15:43:20 +0200

    Solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  let fibArr = [1, 1];
 
+  if (num > 0 && num <= 2) {
+    return 1;
+  } else if (num > 2) {
+    for (let i = 2; i < num; i++) {
+      fibArr.push(fibArr[i - 2] + fibArr[i - 1]);
+    }
+    return fibArr.pop();
+  } else {
+    return "OOPS";
+  }
 };
 
 // Do not edit below this line
