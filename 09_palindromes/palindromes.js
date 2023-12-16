254f63b39b819568c846f5b11653a9b221fb7cbf
TBS-PC <tsahibspele@gmail.com>
2023-12-16 11:28:05 +0200

    Solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const word = str.toLowerCase().match(/\w/g);
+  const rWord = word.slice().reverse();
 
+  return word.join("") === rWord.join("");
 };
 
 // Do not edit below this line
